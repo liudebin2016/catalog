45d655ea1e61a6a73e61766238c1c39e5ced5e5e
liumingchao <mingchao52189914@163.om>
2016-01-09 16:37:12 +0800

    部门岗位信息维护

--- src/main/java/com/jusfoun/catalog/controller/JobContoller.java
+++ src/main/java/com/jusfoun/catalog/controller/JobContoller.java
@@ -1,13 +1,24 @@
 package com.jusfoun.catalog.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.jusfoun.catalog.common.controller.BaseController;
+import com.jusfoun.catalog.common.entity.Page;
+import com.jusfoun.catalog.dao.JobDao;
+import com.jusfoun.catalog.entity.Job;
+import com.jusfoun.catalog.service.JobService;
+import com.jusfoun.catalog.utils.UserUtils;
 
 /**
  * 部门岗位
@@ -16,6 +27,9 @@ import com.jusfoun.catalog.common.controller.BaseController;
  */
 @Controller
 public class JobContoller extends BaseController {
+	
+	@Autowired
+	private JobService jobService;
 
 	/**
 	 * 部门岗位信息列表
@@ -36,8 +50,9 @@ public class JobContoller extends BaseController {
 	 * @return
 	 */
 	@RequestMapping(value = "${adminPath}/job/maintenance", method = RequestMethod.GET)
-	public String getJobMaintenance(HttpServletRequest request, HttpServletResponse response){
-		
+	public String getJobMaintenance(HttpServletRequest request, HttpServletResponse response,Model model){
+		List<Job>jobList = jobService.findJobList();
+		model.addAttribute("jobList", jobList);
 		return "admin/job/jobMaintenance";
 	}
 	
@@ -47,10 +62,21 @@ public class JobContoller extends BaseController {
 	 * @param response
 	 * @return
 	 */
-	@RequestMapping(value = "${adminPath}/job/create", method = RequestMethod.GET)
-	public String getJobCreate(HttpServletRequest request, HttpServletResponse response){
-		
-		return "admin/job/jobMaintenance";
+	@RequestMapping(value = "${adminPath}/job/create", method = RequestMethod.POST)
+	public String getJobCreate(HttpServletRequest request, HttpServletResponse response,Model model){
+		String name = request.getParameter("name");
+		String duty = request.getParameter("duty");
+		String type = request.getParameter("type");
+		Date createDate =new Date();
+		Job job = new Job();
+		job.setName(name);
+		job.setDuty(duty);
+		job.setType(type);
+		job.setCreateBy(UserUtils.getUser());
+		job.setCreateDate(createDate);
+		job.setDelFlag("0");
+		int index = jobService.createJob(job);
+		return getJobMaintenance(request, response, model);
 	}
 	
 	/**
@@ -72,8 +98,36 @@ public class JobContoller extends BaseController {
 	 * @return
 	 */
 	@RequestMapping(value = "${adminPath}/job/edit", method = RequestMethod.GET)
-	public String getJobEdit(HttpServletRequest request, HttpServletResponse response){
-		
+	public String getJobEdit(HttpServletRequest request, HttpServletResponse response,Model model){
+		String id = request.getParameter("id");
+		Job job =jobService.selectById(id);
+		model.addAttribute("job",job);
 		return "admin/job/jobEdit";
+	}
+	
+	/**
+	 * 部门岗位新建按钮
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping(value = "${adminPath}/job/addjob", method = RequestMethod.GET)
+	public String getJobAdd(HttpServletRequest request, HttpServletResponse response,Model model){
+		Job job = new Job();
+		model.addAttribute("job", job);
+		return "admin/job/jobEdit";
+	}
+	/**
+	 * 部门岗位编辑
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping(value = "${adminPath}/job/doedit", method = RequestMethod.POST)
+	public String getJobDoEdit(HttpServletRequest request, HttpServletResponse response,Job job,Model model){
+		job.setUpdateBy(UserUtils.getUser());
+		job.setUpdateDate(new Date());
+		boolean flag = jobService.updateById(job);
+		return getJobMaintenance(request, response, model);
 	}
 }
--- src/main/java/com/jusfoun/catalog/dao/JobDao.java
+++ src/main/java/com/jusfoun/catalog/dao/JobDao.java
@@ -1,5 +1,7 @@
 package com.jusfoun.catalog.dao;
 
+import java.util.List;
+
 import com.jusfoun.catalog.common.annotation.MyBatisDao;
 import com.jusfoun.catalog.common.dao.CrudDao;
 import com.jusfoun.catalog.entity.Job;
@@ -11,5 +13,13 @@ import com.jusfoun.catalog.entity.Job;
  */
 @MyBatisDao
 public interface JobDao extends CrudDao<Job>{
+
+	public int createJob(Job job);
+
+	public List<Job> findJobList();
+
+	public Job selectById(String id);
+
+	public int updateById(Job job);
 
 }
