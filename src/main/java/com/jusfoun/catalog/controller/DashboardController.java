a0039178a592e7ed06a569309522c5c60a8928c2
liudebin2016 <527627490@qq.com>
2016-02-26 18:06:13 +0800

    代码优化

--- src/main/java/com/jusfoun/catalog/controller/DashboardController.java
+++ src/main/java/com/jusfoun/catalog/controller/DashboardController.java
@@ -86,106 +86,171 @@ public class DashboardController extends BaseController{
 	public String search(@RequestParam(value = "search_value", required=true) String value,
 			@RequestParam(value = "search_type", required=true) int type,
 			@RequestParam(value = "officeId", required=false) Integer officeId) {
-    	SearchLog log = new SearchLog();
-    	log.setKeyword(value);
-    	log.setSrhTime(new Date());
-    	log.setSrhType(type);
-    	searchLogService.save(log);
-    	Map<String,Object> param = new HashMap<String, Object>();
-    	param.put("duty", value.trim());
-    	if(officeId!=null){
-    		param.put("id", officeId);
+    	String ofclStr="{}";
+    	if(!StringUtils.isEmpty(value)){
+    		SearchLog log = new SearchLog();
+    		log.setKeyword(value);
+    		log.setSrhTime(new Date());
+    		log.setSrhType(type);
+    		if(officeId!=null){
+    			log.setOfficeId(officeId);
+    		}
+    		searchLogService.save(log);
+    		
+    		Map<String,Object> param = new HashMap<String, Object>();
+    		param.put("duty", value.trim());
+    		if(officeId!=null){
+    			param.put("id", officeId);
+    		}
+    		List<Office> officeList = officeService.queryOffices(param);
+    		for(Office office : officeList){
+    			office.setDuty(StringEscapeUtils.unescapeHtml4(office.getDuty().replaceAll(value.trim(), "&lt;span style='color:orange'&gt;"+value.trim()+"&lt;/span&gt;")));
+    		}
+    		ofclStr=JsonMapper.toJsonString(officeList);
     	}
-    	List<Office> officeList = officeService.queryOffices(param);
-    	for(Office office : officeList){
-    		office.setDuty(StringEscapeUtils.unescapeHtml4(office.getDuty().replaceAll(value.trim(), "&lt;span style='color:orange'&gt;"+value.trim()+"&lt;/span&gt;")));
-    	}
-    	String ofclStr=JsonMapper.toJsonString(officeList);
 		return ofclStr;
 	}
     
     @ResponseBody
     @RequestMapping(value = "/srh/jobDg")
     public String srhJobDg(int page,int rows,HttpServletRequest request){
-    	String name=request.getParameter("name");
-    	String officeId=StringUtils.trim(request.getParameter("officeId"));
-    	//求得开始记录与结束记录
-		int start = (page-1)*rows+1;
-		int end = page * rows;
-		Job job=new Job();
-		job.setName(name);
-		if(StringUtils.isNotEmpty(officeId)){
-			job.setOfficeId(Integer.valueOf(officeId));
-		}
-		job.getSqlMap().put("start", ""+start);
-		job.getSqlMap().put("end", ""+end);
-		int jobCount=jobService.findSrhListCount(job);
-    	List<Job> jobList=jobService.findSrhList(job);
-    	String json = JsonMapper.toJsonString(jobList);
-		StringBuffer sb=new StringBuffer();
-		sb.append("{\"total\":").append(jobCount).append(",\"rows\":").append(json).append("}");
-    	return sb.toString();
+    	String name=request.getParameter("name")==null?null:StringUtils.trim(request.getParameter("name"));
+    	String officeId=request.getParameter("officeId")==null?null:StringUtils.trim(request.getParameter("officeId"));
+    	String type=StringUtils.trim(request.getParameter("search_type"));
+    	
+    	String retVal="{\"total\":0,\"rows\":[]}";
+    	if(!StringUtils.isBlank(name)){
+    		SearchLog log = new SearchLog();
+    		log.setKeyword(name);
+    		log.setSrhTime(new Date());
+    		log.setSrhType(Integer.valueOf(type));
+    		if(officeId!=null){
+    			log.setOfficeId(Integer.valueOf(officeId));
+    		}
+    		searchLogService.save(log);
+    		
+    		//求得开始记录与结束记录
+    		int start = (page-1)*rows+1;
+    		int end = page * rows;
+    		Job job=new Job();
+    		job.setName(name);
+    		if(StringUtils.isNotEmpty(officeId)){
+    			job.setOfficeId(Integer.valueOf(officeId));
+    		}
+    		job.getSqlMap().put("start", ""+start);
+    		job.getSqlMap().put("end", ""+end);
+    		int jobCount=jobService.findSrhListCount(job);
+    		List<Job> jobList=jobService.findSrhList(job);
+    		String json = JsonMapper.toJsonString(jobList);
+    		StringBuffer sb=new StringBuffer();
+    		sb.append("{\"total\":").append(jobCount).append(",\"rows\":").append(json).append("}");
+    		retVal=sb.toString();
+    	}
+    	return retVal;
     }
     
     @ResponseBody
     @RequestMapping(value = "/srh/bizDg")
     public String srhBizDg(int page,int rows,HttpServletRequest request){
-    	//求得开始记录与结束记录
-		int start = (page-1)*rows+1;
-		int end = page * rows;
-		Business biz=new Business();
-		String officeId=StringUtils.trim(request.getParameter("officeId"));
-		if(StringUtils.isNotEmpty(officeId)){
-			biz.setChargeOfficeId(Long.valueOf(officeId));
-		}
-		biz.setName(request.getParameter("name"));
-		biz.getSqlMap().put("start", ""+start);
-		biz.getSqlMap().put("end", ""+end);
-    	int bizCount=businessService.findSrhListCount(biz);
-    	List<Business> bizList=businessService.findSrhList(biz);
-    	String json = JsonMapper.toJsonString(bizList);
-		StringBuffer sb=new StringBuffer();
-		sb.append("{\"total\":").append(bizCount).append(",\"rows\":").append(json).append("}");
-    	return sb.toString();
+    	String officeId=request.getParameter("officeId")==null?null:StringUtils.trim(request.getParameter("officeId"));
+    	String type=StringUtils.trim(request.getParameter("search_type"));
+    	String name=request.getParameter("name")==null?null:StringUtils.trim(request.getParameter("name"));
+    	
+    	String retVal="{\"total\":0,\"rows\":[]}";
+    	if(!StringUtils.isBlank(name)){
+    		SearchLog log = new SearchLog();
+    		log.setKeyword(name);
+    		log.setSrhTime(new Date());
+    		log.setSrhType(Integer.valueOf(type));
+    		if(officeId!=null){
+    			log.setOfficeId(Integer.valueOf(officeId));
+    		}
+    		searchLogService.save(log);
+    		
+    		//求得开始记录与结束记录
+    		int start = (page-1)*rows+1;
+    		int end = page * rows;
+    		Business biz=new Business();
+    		if(StringUtils.isNotEmpty(officeId)){
+    			biz.setChargeOfficeId(Long.valueOf(officeId));
+    		}
+    		biz.setName(name);
+    		biz.getSqlMap().put("start", ""+start);
+    		biz.getSqlMap().put("end", ""+end);
+    		int bizCount=businessService.findSrhListCount(biz);
+    		List<Business> bizList=businessService.findSrhList(biz);
+    		String json = JsonMapper.toJsonString(bizList);
+    		StringBuffer sb=new StringBuffer();
+    		sb.append("{\"total\":").append(bizCount).append(",\"rows\":").append(json).append("}");
+    		retVal=sb.toString();
+    	}
+    	return retVal;
     }
     
     @ResponseBody
     @RequestMapping(value = "/srh/rscDg")
     public String srhRscDg(int page,int rows,HttpServletRequest request){
-    	String officeId=StringUtils.trim(request.getParameter("officeId"));
-    	//求得开始记录与结束记录
-		int start = (page-1)*rows+1;
-		int end = page * rows;
-		ResourceInfo rsc=new ResourceInfo();
-		if(StringUtils.isNotEmpty(officeId)){
-			rsc.setResponseParty(officeId);
-		}
-		rsc.setName(request.getParameter("name"));
-		rsc.getSqlMap().put("start", ""+start);
-		rsc.getSqlMap().put("end", ""+end);
-    	List<ResourceInfo> rscList=resourceService.findSrhList(rsc);
-    	int rscCount=resourceService.findSrhListCount(rsc);
-    	String json = JsonMapper.toJsonString(rscList);
-		StringBuffer sb=new StringBuffer();
-		sb.append("{\"total\":").append(rscCount).append(",\"rows\":").append(json).append("}");
-    	return sb.toString();
+    	String officeId=request.getParameter("officeId")==null?null:StringUtils.trim(request.getParameter("officeId"));
+    	String type=StringUtils.trim(request.getParameter("search_type"));
+    	String name=request.getParameter("name")==null?null:StringUtils.trim(request.getParameter("name"));
+    	String retVal="{\"total\":0,\"rows\":[]}";
+    	if(!StringUtils.isBlank(name)){
+    		SearchLog log = new SearchLog();
+    		log.setKeyword(name);
+    		log.setSrhTime(new Date());
+    		log.setSrhType(Integer.valueOf(type));
+    		if(officeId!=null){
+    			log.setOfficeId(Integer.valueOf(officeId));
+    		}
+    		searchLogService.save(log);
+    		
+    		//求得开始记录与结束记录
+    		int start = (page-1)*rows+1;
+    		int end = page * rows;
+    		ResourceInfo rsc=new ResourceInfo();
+    		if(StringUtils.isNotEmpty(officeId)){
+    			rsc.setResponseParty(officeId);
+    		}
+    		rsc.setName(name);
+    		rsc.getSqlMap().put("start", ""+start);
+    		rsc.getSqlMap().put("end", ""+end);
+    		List<ResourceInfo> rscList=resourceService.findSrhList(rsc);
+    		int rscCount=resourceService.findSrhListCount(rsc);
+    		String json = JsonMapper.toJsonString(rscList);
+    		StringBuffer sb=new StringBuffer();
+    		sb.append("{\"total\":").append(rscCount).append(",\"rows\":").append(json).append("}");
+    		retVal=sb.toString();
+    	}
+    	return retVal;
     }
     
     @ResponseBody
     @RequestMapping(value = "/srh/subDg")
     public String srhSubDg(int page,int rows,HttpServletRequest request){
-    	//求得开始记录与结束记录
-		int start = (page-1)*rows+1;
-		int end = page * rows;
-		SubjectInfo sub=new SubjectInfo();
-		sub.setName(request.getParameter("name"));
-		sub.getSqlMap().put("start", ""+start);
-		sub.getSqlMap().put("end", ""+end);
-    	List<ResourceInfo> rscList=subjectService.findSrhList(sub);
-    	int rscCount=subjectService.findSrhListCount(sub);
-    	String json = JsonMapper.toJsonString(rscList);
-		StringBuffer sb=new StringBuffer();
-		sb.append("{\"total\":").append(rscCount).append(",\"rows\":").append(json).append("}");
-    	return sb.toString();
+    	String type=StringUtils.trim(request.getParameter("search_type"));
+    	String name=request.getParameter("name")==null?null:StringUtils.trim(request.getParameter("name"));
+    	String retVal="{\"total\":0,\"rows\":[]}";
+    	if(!StringUtils.isBlank(name)){
+    		SearchLog log = new SearchLog();
+    		log.setKeyword(name);
+    		log.setSrhTime(new Date());
+    		log.setSrhType(Integer.valueOf(type));
+    		searchLogService.save(log);
+    		
+    		//求得开始记录与结束记录
+    		int start = (page-1)*rows+1;
+    		int end = page * rows;
+    		SubjectInfo sub=new SubjectInfo();
+    		sub.setName(name);
+    		sub.getSqlMap().put("start", ""+start);
+    		sub.getSqlMap().put("end", ""+end);
+    		List<ResourceInfo> rscList=subjectService.findSrhList(sub);
+    		int rscCount=subjectService.findSrhListCount(sub);
+    		String json = JsonMapper.toJsonString(rscList);
+    		StringBuffer sb=new StringBuffer();
+    		sb.append("{\"total\":").append(rscCount).append(",\"rows\":").append(json).append("}");
+    		retVal=sb.toString();
+    	}
+    	return retVal;
     }
 }
--- src/main/java/com/jusfoun/catalog/entity/SearchLog.java
+++ src/main/java/com/jusfoun/catalog/entity/SearchLog.java
@@ -4,7 +4,8 @@ import java.util.Date;
 
 import com.jusfoun.catalog.common.entity.DataEntity;
 
-/**搜索日志
+/**
+ * 搜索日志
  * @author xuym
  *
  */
@@ -12,24 +13,25 @@ public class SearchLog extends DataEntity<SearchLog> {
 	
 	private static final long serialVersionUID = 1L;
 	
-	public static final int TYPE_OFFICE = 1;
-	public static final int TYPE_JOB = 2;
-	public static final int TYPE_BUSINESS = 3;
-	public static final int TYPE_RESOURCE = 4;
-	public static final int TYPE_SUBJECT = 5;
+	/******************搜索类型值--start********************************/
+	public static final int TYPE_OFFICE = 1;//机构
+	public static final int TYPE_JOB = 2;//岗位
+	public static final int TYPE_BUSINESS = 3;//业务
+	public static final int TYPE_RESOURCE = 4;//资源
+	public static final int TYPE_SUBJECT = 5;//主题
+	/******************搜索类型值--end**********************************/
 	
-	/**
-	 * 搜索关键词
-	 */
-	private String keyword;
-	/**
-	 * 搜索日期
-	 */
-	private Date srhTime;
-	/**
-	 * 搜索类型
-	 */
-	private Integer srhType;
+	private String keyword;//搜索关键词
+	private Date srhTime;//搜索日期
+	private Integer srhType;//搜索类型	
+	private Integer officeId;//机构id
+	
+	public Integer getOfficeId() {
+		return officeId;
+	}
+	public void setOfficeId(Integer officeId) {
+		this.officeId = officeId;
+	}
 	public String getKeyword() {
 		return keyword;
 	}
